1ad7d0e6adae233817cda14899710c412e78e9ca
Yuan <jieyuan.zhao@ninebot.com>
2023-05-17 09:44:38 +0800

    [fix](demo): 测试调用  - test

---
+++ demo.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * @Message
+ * @Author: Yuan
+ * @Time: 2023/5/17 09:40
+ */
+function __construct()
+{
+
+}
+
+function test()
+{
+    return "test is true";
+}
+
+function testParams($string)
+{
+    return "test output is " . $string;
+}
+
+function __destruct()
+{
+
+}
